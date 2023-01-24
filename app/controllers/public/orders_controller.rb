042b7833e3e90003870b018a9c7c51849d55e7e9
EC2 Default User <ec2-user@ip-172-31-39-139.ap-northeast-1.compute.internal>
2023-01-24 13:50:05 +0000

    nagano-cake

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,19 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def confirm
+  end
+
+  def completion
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
     root to: "public/homes#top"
-  scope module: :public do
-    get "about"=>"homes#about"
-    resources :items, only: [:index, :show]
-devise_for :customers,skip: [:passwords], controllers: {
+  devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  scope module: :public do
+    get "about"=>"homes#about"
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :uppdate]
+    get "customers/confrim_withdraw"
+    get "customers/withdraw"
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only: [:new, :confrim, :completion, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
 end
   namespace :admin do
     root "homes#top"
@@ -16,8 +25,5 @@ end
     get 'orders/show'
     get 'orders_details/update'
   end
-devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
